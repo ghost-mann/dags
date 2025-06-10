5612b401037675eaaf7d77af0f7ae9edda175a77
ghost-mann <iamaob8@protonmail.com>
2025-06-10 22:06:18 +0300

    feat: added aiven db variables in yt_dag.py

--- yt_dag.py
+++ yt_dag.py
@@ -1,7 +1,6 @@
 from airflow import DAG
 import os
-import json
-import sys
+import psycopg2
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from dotenv import load_dotenv
@@ -11,7 +10,15 @@ from googleapiclient.discovery import build
 load_dotenv()
 
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
-# CHANNEL_ID = os.getenv("CHANNEL_ID")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+AIVEN_DB_CONFIG = {
+    'host':os.getenv('AIVEN_DB_HOST'),
+    'database_name':os.getenv('AIVEN_DB_NAME'),
+    'user':os.getenv('AIVEN_USER'),
+    'password':os.getenv('AIVEN_PASSWORD'),
+    'port':os.getenv('AIVEN_PORT')
+}
 
 # yt api connection
 youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
